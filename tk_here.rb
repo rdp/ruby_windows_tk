e898e02afae3026bbf5367c55dd7a4311f643327
rogerdpack <rogerpack2005@gmail.com>
2009-06-24 14:09:10 -0600

    allow it to actually work

--- tk_here.rb
+++ tk_here.rb
@@ -2,5 +2,5 @@ dirname = File.dirname(File.expand_path(__FILE__))
 for dir in ['/lib/ruby/1.8', '/lib/ruby/1.8/tkextlib', '/lib/ruby/1.8/i386-mswin32'] do
   $: << dirname + dir
 end
-require dirname + "/lib/ruby/1.8/tk.rb"
-Tk
+require "tk.rb"
+a = Tk
